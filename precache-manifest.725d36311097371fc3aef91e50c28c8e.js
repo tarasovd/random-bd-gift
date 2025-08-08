66c3811e24b3c87bbb01c37a4ff05b646c23b140
Dmitrijs <dmitry.tarasov4@gmail.com>
2025-08-08 18:49:46 +0300

    Updates

---
+++ precache-manifest.725d36311097371fc3aef91e50c28c8e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e5f84ea1ce501be0c1b1aa03edd1e09",
+    "url": "/random-bd-gift/index.html"
+  },
+  {
+    "revision": "658cfcd75a099c364064",
+    "url": "/random-bd-gift/static/css/main.b8da04aa.chunk.css"
+  },
+  {
+    "revision": "380be80c6e7b0e671c22",
+    "url": "/random-bd-gift/static/js/2.a3f78a0f.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/random-bd-gift/static/js/2.a3f78a0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a54163d935e7f35e98",
+    "url": "/random-bd-gift/static/js/3.fc04cec8.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/random-bd-gift/static/js/3.fc04cec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "658cfcd75a099c364064",
+    "url": "/random-bd-gift/static/js/main.cf509596.chunk.js"
+  },
+  {
+    "revision": "90cc87c89e5869e2b419",
+    "url": "/random-bd-gift/static/js/runtime-main.d2c85155.js"
+  },
+  {
+    "revision": "a1b484106cbf24d41bd70ded93eb2198",
+    "url": "/random-bd-gift/static/media/baraban.a1b48410.mp3"
+  },
+  {
+    "revision": "a1fcb1ccf729175d81eec89af257563b",
+    "url": "/random-bd-gift/static/media/fanfary.a1fcb1cc.mp3"
+  },
+  {
+    "revision": "cff6a31da34360844ba25be1a9a43110",
+    "url": "/random-bd-gift/static/media/win.cff6a31d.gif"
+  }
+]);
